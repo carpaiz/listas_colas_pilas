de97e1d60882a6226708fe8c861951696c98bb68
carpaiz <paiz2790@gmail.com>
2022-02-17 17:32:46 -0600

    primer commite

---
+++ src/listas_colas_pilas/Listas_colas_pilas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listas_colas_pilas;
+
+/**
+ *
+ * @author Desarrollo
+ */
+public class Listas_colas_pilas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_1 frm = new frm_1();
+        frm.show();
+        
+        
+    }
+    
+}
